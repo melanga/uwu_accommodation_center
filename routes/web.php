4b55f9c58fcfc619ee1edfb5878b7c5a2e752410
Melanga Dissanayake <dissanayakedmmb@gmail.com>
2022-08-25 13:06:11 +0530

    added breeze starter templates

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/student', [StudentController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
